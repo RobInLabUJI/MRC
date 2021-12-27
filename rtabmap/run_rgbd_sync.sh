1d651878a1944c8c2ccc053a6b536d9fa73a6d09
ecervera <Enric Cervera>
2021-12-27 10:05:41 +0100

    Uncompressed

--- rtabmap/run_rgbd_sync.sh
+++ rtabmap/run_rgbd_sync.sh
@@ -2,7 +2,7 @@
 
 rocker --nvidia --x11 --env USER=ecervera \
   --env ROS_IP=169.254.65.116 \
-  --env ROS_MASTER_URI=http://169.254.208.115:11311 \
+  --env ROS_MASTER_URI=http://169.254.22.178:11311 \
   --volume /tmp/.X11-unix:/tmp/.X11-unix --volume /dev:/dev:ro \
   --volume /lib/modules:/lib/modules:ro --volume /tmp:/tmp:rw \
   --volume $PWD/.ros:/root/.ros:rw \
@@ -10,5 +10,5 @@ rocker --nvidia --x11 --env USER=ecervera \
   roslaunch rtabmap_ros rtabmap.launch \
     subscribe_rgbd:=true \
     rgbd_topic:=/camera/rgbd_image \
-    compressed:=true \
+    compressed:=false \
     rtabmap_args:="--delete_db_on_start"
