cee6ab3a4559d3dc7a073481ecf44d4a059a5d93
ecervera <Enric Cervera>
2021-12-27 09:15:41 +0100

    Use rgbd_sync

---
+++ rtabmap/run_rgbd_sync.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+rocker --nvidia --x11 --env USER=ecervera \
+  --env ROS_IP=169.254.65.116 \
+  --env ROS_MASTER_URI=http://169.254.208.115:11311 \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix --volume /dev:/dev:ro \
+  --volume /lib/modules:/lib/modules:ro --volume /tmp:/tmp:rw \
+  --volume $PWD/.ros:/root/.ros:rw \
+  --network host robinlab/cmr_rtabmap \
+  roslaunch rtabmap_ros rtabmap.launch \
+    subscribe_rgbd:=true \
+    rgbd_topic:=/camera/rgbd_image \
+    compressed:=true
+    rtabmap_args:="--delete_db_on_start"
