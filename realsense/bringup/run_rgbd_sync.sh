a2f8b1260ad92799d696eeffd2aa2e4847f9fc92
Enric Cervera <ecervera@uji.es>
2021-12-27 09:07:58 +0100

    Add rgbd_sync

--- realsense/bringup/run_rgbd_sync.sh
+++ realsense/bringup/run_rgbd_sync.sh
@@ -1,4 +1,6 @@
 docker run --rm -it --privileged --network host \
   --env ROS_IP=169.254.208.115 \
   --env ROS_MASTER_URI=http://169.254.208.115:11311 \
-  robinlab/cmr_realsense_bringup
+  --volume $PWD/.ros:/root/.ros:rw \
+  robinlab/cmr_realsense_bringup \
+  roslaunch /realsense.launch
