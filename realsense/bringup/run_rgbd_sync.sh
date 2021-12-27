80711303bb37222f4158c5cf4261156838e1de24
Enric Cervera <ecervera@uji.es>
2021-12-27 10:06:51 +0100

    Set compressed rate

--- realsense/bringup/run_rgbd_sync.sh
+++ realsense/bringup/run_rgbd_sync.sh
@@ -1,6 +1,6 @@
 docker run --rm -it --privileged --network host \
-  --env ROS_IP=169.254.208.115 \
-  --env ROS_MASTER_URI=http://169.254.208.115:11311 \
+  --env ROS_IP=169.254.22.178 \
+  --env ROS_MASTER_URI=http://169.254.22.178:11311 \
   --volume $PWD/.ros:/root/.ros:rw \
   robinlab/cmr_realsense_bringup \
-  roslaunch /realsense.launch
+  roslaunch /realsense.launch rate:=15
